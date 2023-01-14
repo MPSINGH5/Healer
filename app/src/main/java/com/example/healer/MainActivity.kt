ee85faa4354c1502e615bf131f01ea2c53ff2631
Manvendra <mannupratapsingh@gmail.com>
2023-01-15 01:12:49 +0530

    Created xml for frag2 and fixed status bar light icons problem

--- app/src/main/java/com/example/healer/MainActivity.kt
+++ app/src/main/java/com/example/healer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
