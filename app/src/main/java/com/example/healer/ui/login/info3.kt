98c8306cee0412884c3a537421f295e76946bd2f
Manvendra <mannupratapsingh@gmail.com>
2023-03-07 22:15:29 +0530

    Final structure

--- app/src/main/java/com/example/healer/ui/login/info3.kt
+++ app/src/main/java/com/example/healer/ui/login/info3.kt
@@ -1,4 +1,4 @@
-package com.example.healer.ui.home
+package com.example.healer.ui.login
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [chats.newInstance] factory method to
+ * Use the [info3.newInstance] factory method to
  * create an instance of this fragment.
  */
-class chats : Fragment() {
+class info3 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class chats : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false)
+        return inflater.inflate(R.layout.fragment_info3, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class chats : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment chats.
+         * @return A new instance of fragment info3.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            chats().apply {
+            info3().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
